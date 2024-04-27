8ada827b371ede87483b858b10fa37452b44c5e9
Rasciel Villegas <rak.alex90@gmail.com>
2024-04-27 20:35:03 +0200

    refactor(app.component): remove default set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,5 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'world-ranks';
-}
+export class AppComponent {}
