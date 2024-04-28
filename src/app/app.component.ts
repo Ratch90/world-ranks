5c5ebe889bbcb007ff6c7a685b2798399e778f89
Rasciel Villegas <rak.alex90@gmail.com>
2024-04-28 20:32:41 +0200

    feat(app.component): set an initial prototype for the layout in the template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/layout';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, HeaderComponent],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {}
---
+++ src/app/shared/components/layout/index.ts
@@ -0,0 +1,3 @@
+export * from './header/header.component';
+export * from './main-content/main-content.component';
+export * from './logo/logo.component';
