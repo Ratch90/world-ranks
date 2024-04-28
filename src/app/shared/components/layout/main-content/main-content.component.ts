087795421783f98b4f8f1719bdadf0c0add342a7
Rasciel Villegas <rak.alex90@gmail.com>
2024-04-28 20:25:30 +0200

    feat: create main content component

---
+++ src/app/shared/components/layout/main-content/main-content.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-main-content',
+    templateUrl: './main-content.component.html',
+    standalone: true
+})
+export class MainContentComponent {}
