cd2ffbf82ad26b9215c9d04444ad046f9c3a50b0
Rasciel Villegas <rak.alex90@gmail.com>
2024-05-02 15:36:35 +0200

    refactor: remove main content component

--- src/app/shared/components/layout/main-content/main-content.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-main-content',
-    templateUrl: './main-content.component.html',
-    standalone: true
-})
-export class MainContentComponent {}
